1a37fd80e00d7dc6a04c0f896c53a39cb00e13c7
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-01 16:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,84 @@
-(function () {
-
+function () {
     'use strict';
     
     let convertType = "miles";
-    
     const heading = document. querySelector('h1');
-    const intro = document.querySelector( 'p');
-    const answerDiv = document.getElementById( 'answer' );
-    const form = document.getElementById( 'convert' );
+    const intro = document.querySelector('p');
+    const answerDiv = document.getElementById('answer');
+    const form = document.getElementById('convert');
     
-    document.addEventListener('keydown', function (event)) {
+    document.addEventListener('keydown', function (event) {
     
-    var key = event. code;
-    
+    var key = event.code;
+
     if (key === 'KeyK') {
     convertType = 'kilometers';
-    heading. innerHTML = "Kilometers to Miles Converter";
-    intro. innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles"
+    heading.innerHTML = "Kilometers to Miles Converter";
+    intro.innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles.";
     
     }
     
-    else if (key =as 'KeyM') {
-    
+    else if (key = 'KeyM') {
     convertType = 'miles';
-    
     heading. InnerHTML = "Miles to Kilometers Converter";
+    intro. innerHTML = "Type in a number of miles and click the button to convert thedistance to kilometers.";
     
+    }   
+
+   'use strict';
+    
+    let convertType = "miles";
+    const heading = document. querySelector('h1');
+    const intro = document.querySelector('p');
+    const answerDiv = document.getElementById('answer');
+    const form = document.getElementById('convert');
+    
+    document.addEventListener('keydown', function (event) {
+    
+    var key = event.code;
+
+    if (key === 'KeyK') {
+    convertType = 'kilometers';
+    heading.innerHTML = "Kilometers to Miles Converter";
+    intro.innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles.";
+    
+    }
+    
+    else if (key = 'KeyM') {
+    convertType = 'miles';
+    heading. InnerHTML = "Miles to Kilometers Converter";
     intro. innerHTML = "Type in a number of miles and click the button to convert thedistance to kilometers.";
     
     }
-    
-    });
-    
+});
     
     form. addEventListener ('submit', function (event) {
-    
-    event.preventDefault ();
-    
-    const distance = parseFloat(document.getElementById( 'distance' ) . value);
+
+    event.preventDefault();
+
+    const distance = parseFloat(document.getElementById('distance').value);
     
     
     if (distance) {
-    
     // convert M to K 1.609344
-    
     // convert K to M 0.621371192
-    
     if (convertType == "miles") {
     const converted = (distance * 1.609344). toFixed(3);
-    answerDiv. innerHTML = ${distance} miles converts to ${converted} 'kilometers';
-    
-    }
+    answerDiv.innerHTML = '${distance} miles converts to ${converted} kilometers';
     
     }
     
     else {
     const converted = (distance * 0.621371192). toFixed(3);
-    answerDiv. innerHTML = ${distance} kilometers converts to ${converted} 'miles';
+    answerDiv. innerHTML = '${distance} kilometers converts to ${converted} miles';
     
     }
+        
+        }
     
     else {
-    answerDiv. innerHTML = "<h2>Please provide a number!</h2>";
+    answerDiv.innerHTML = "<h2>Please provide a number!</h2>";
+
     }
-    
-    });
+});
+});
