b282516e59bb23d8cd0a1e2fabe436a0644957ce
Jean-Joooo <124114074+Jean-Joooo@users.noreply.github.com>
2024-01-02 01:36:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,43 +20,18 @@ function () {
     
     else if (key = 'KeyM') {
     convertType = 'miles';
-    heading. InnerHTML = "Miles to Kilometers Converter";
-    intro. innerHTML = "Type in a number of miles and click the button to convert thedistance to kilometers.";
+    heading.innerHTML = "Miles to Kilometers Converter";
+    intro.innerHTML = "Type in a number of miles and click the button to convert thedistance to kilometers.";
     
     }   
 
-   'use strict';
-    
-    let convertType = "miles";
-    const heading = document. querySelector('h1');
-    const intro = document.querySelector('p');
-    const answerDiv = document.getElementById('answer');
-    const form = document.getElementById('convert');
-    
-    document.addEventListener('keydown', function (event) {
-    
-    var key = event.code;
+    });
 
-    if (key === 'KeyK') {
-    convertType = 'kilometers';
-    heading.innerHTML = "Kilometers to Miles Converter";
-    intro.innerHTML = "Type in a number of kilometers and click the button to convert the distance to miles.";
-    
-    }
-    
-    else if (key = 'KeyM') {
-    convertType = 'miles';
-    heading. InnerHTML = "Miles to Kilometers Converter";
-    intro. innerHTML = "Type in a number of miles and click the button to convert thedistance to kilometers.";
-    
-    }
-});
-    
-    form. addEventListener ('submit', function (event) {
+   form.addEventListener ('submit', function (event) {
 
     event.preventDefault();
 
-    const distance = parseFloat(document.getElementById('distance').value);
+    var distance = parseFloat(document.getElementById('distance').value);
     
     
     if (distance) {
